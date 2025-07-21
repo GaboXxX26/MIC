322dd9a787d86c542eb2aab55a7717c234393362
Gabriel Davila <114791590+GaboXxX26@users.noreply.github.com>
2025-07-21 10:11:05 -0500

    Cambios a JWT
    se cambio un nuevo proyecto con JWT, solo funciona el login faltan el resto, .txt tiene u poco de info para compilar

---
+++ MIC/app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Credenciales inválidas'], 401);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function me()
+    {
+        return response()->json(JWTAuth::parseToken()->authenticate());
+    }
+}
